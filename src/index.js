cec1592a32da8282b5ec35fff65c1ac382a27b3c
Jesse Baber <jesse.baber@idnet.com>
2019-04-06 01:47:10 +0700

    updated web.config deployment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
-import "./web.config";
 ReactDOM.render(<App />, document.getElementById("root"))
